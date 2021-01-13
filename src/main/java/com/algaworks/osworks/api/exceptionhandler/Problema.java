3c8f3091c28aeb9569ee4a95d139fbb423361c2f
Vitor Fantoni Cutulo <global.vitor@ao3tech.com>
2021-01-13 19:38:07 -0300

    persistência com MySQL, criação do CRUD de clientes e tratamento de exceptions/retornos

---
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/Problema.java
@@ -0,0 +1,34 @@
+package com.algaworks.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Data;
+
+@Data
+@JsonInclude(Include.NON_NULL)
+public class Problema {
+	
+	private Integer status;
+	private LocalDateTime dataHora;
+	private String titulo;
+	private List<Campo> campos;
+	
+	@Data
+	public static class Campo {
+		
+		private String nome;
+		private String mensagem;
+		
+		public Campo(String nome, String mensagem) {
+			super();
+			this.nome = nome;
+			this.mensagem = mensagem;
+		}
+		
+	}
+
+}
--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -1,13 +1,39 @@
 package com.algaworks.osworks.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
 	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
+	
+	@Email
+	@NotBlank
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
+	@Column(name = "fone")
 	private String telefone;
 	
 }
